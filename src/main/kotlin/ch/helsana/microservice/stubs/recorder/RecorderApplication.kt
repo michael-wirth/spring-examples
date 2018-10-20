52d47cd3ecca5b923dc9e2c5585d3c937c9471b1
Michael Wirth <michael.wirth@mimacom.com>
2018-10-20 18:28:24 +0200

    fix(service): update commandline

--- src/main/kotlin/ch/helsana/microservice/stubs/recorder/RecorderApplication.kt
+++ src/main/kotlin/ch/helsana/microservice/stubs/recorder/RecorderApplication.kt
@@ -1,6 +1,9 @@
 package ch.helsana.microservice.stubs.recorder
 
 import ch.helsana.microservice.stubs.recorder.client.ApiBridgeClient
+import com.adcubum.syrius.api.bestandsverw.common.identifier.v1.WsPartnerIdType
+import com.adcubum.syrius.api.partnermgmt.common.identifier.v1.WsLeistungserbringerIdType
+import com.adcubum.syrius.api.produktmgmt.common.identifier.v1.WsProduktIdType
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -14,13 +17,40 @@ class RecorderApplication : CommandLineRunner {
     override fun run(vararg args: String?) {
         val partnerId = apiBridgeClient.resolvePortalKontoId("A1031934")
         partnerId?.apply {
-            println(apiBridgeClient.getPartner(this))
+            val partner = apiBridgeClient.getPartner(this)
+            val adresse = apiBridgeClient.getDomizilAdressse(this)
+            val email = apiBridgeClient.getEmail(this)
+            val telefon = apiBridgeClient.getTelefon(this)
+            val organisationseinheit = apiBridgeClient.getOrganisationseinheit(this)
+            val organisationseinheitTelefon = apiBridgeClient.getOrganisationseinheitTelefon(organisationseinheit.first().id)
             val familie = apiBridgeClient.getFamilie(this)
-            println("vorstand")
-            println(familie.vorstand.intBetreuerId.id)
-            println("familie")
-            familie.mitglieders.stream().map { it.intBetreuerId.id }.forEach { println(it) }
-
+            val vertrag = apiBridgeClient.getVertrag(WsPartnerIdType(this.id))
+            val versichertenkarte = apiBridgeClient.getVersichertenkarte(WsPartnerIdType(this.id))
+            val kostenbeteiligung = apiBridgeClient.getKostenbeteiligung(com.adcubum.syrius.api.schadenleistungsmgmt.extern.partner.identifier.v1.WsPartnerIdType(this.id))
+            val versichertesObjekt = apiBridgeClient.getVersichertesObjekt(WsPartnerIdType(this.id))
+            val produkte = apiBridgeClient.getProdukt(*versichertesObjekt
+                    .flatMap { it.vertragsproduktes }
+                    .map { WsProduktIdType(it.produktId.id) }
+                    .toTypedArray())
+            val leistungserbringer = apiBridgeClient.getLeistungserbringer(*versichertesObjekt
+                    .flatMap { it.vertragsproduktes }
+                    .map { it.leistungserbringerId.id }
+                    .filterNot { it == "" }
+                    .map { WsLeistungserbringerIdType(it) }
+                    .toTypedArray())
+            println("partner: $partner")
+            println("adresse: ${adresse.size}")
+            println("email: ${email.size}")
+            println("telefon: ${telefon.size}")
+            println("organisationseinheit: ${organisationseinheit.size}")
+            println("organisationseinheitTelefon: ${organisationseinheitTelefon.size}")
+            println("familie: ${familie.mitglieders.size}")
+            println("vertrag: ${vertrag.size}")
+            println("versichertenkarte: ${versichertenkarte.size}")
+            println("kostenbeteiligung: ${kostenbeteiligung.size}")
+            println("versichertesObjekt: ${versichertesObjekt.size}")
+            println("produkte: ${produkte.size}")
+            println("leistungserbringer: ${leistungserbringer.size}")
         }
     }
 }
--- src/main/kotlin/ch/helsana/microservice/stubs/recorder/client/ApiBridgeClient.kt
+++ src/main/kotlin/ch/helsana/microservice/stubs/recorder/client/ApiBridgeClient.kt
@@ -1,6 +1,41 @@
 package ch.helsana.microservice.stubs.recorder.client
 
+import com.adcubum.syrius.api.bestandsverw.common.identifier.v1.WsKollektivvertragNamentlichIdType
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertenkarte.v1.schema.GetVersichertenkarteRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertenkarte.v1.schema.GetVersichertenkarteResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertenkarte.v1.schema.SearchAktuelleVersichertenkarteForPartnerRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertenkarte.v1.schema.SearchAktuelleVersichertenkarteForPartnerResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertenkarte.v1.schema.WsVersichertenkarteType
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertesobjekt.v1.schema.GetVersichertesObjektRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertesobjekt.v1.schema.GetVersichertesObjektResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertesobjekt.v1.schema.SearchVersichertesObjektForPartnerRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertesobjekt.v1.schema.SearchVersichertesObjektForPartnerResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.versichertesobjekt.v1.schema.WsVersichertesObjektType
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.GetKollektivvertragNamentlichRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.GetKollektivvertragNamentlichResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.GetVertragRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.GetVertragResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.SearchVertragForPartnerRequest
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.SearchVertragForPartnerResponse
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.WsKollektivvertragNamentlichType
+import com.adcubum.syrius.api.bestandsverw.vertragsverw.auskunft.data.vertrag.v1.schema.WsVertragType
+import com.adcubum.syrius.api.partnermgmt.common.identifier.v1.WsLeistungserbringerIdType
+import com.adcubum.syrius.api.partnermgmt.common.identifier.v1.WsOrganisationseinheitIdType
 import com.adcubum.syrius.api.partnermgmt.common.identifier.v1.WsPartnerIdType
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.adresse.v1.schema.GetPartnerDomiziladresseRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.adresse.v1.schema.GetPartnerDomiziladresseResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.adresse.v1.schema.WsPartnerDomiziladresseType
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.email.v1.schema.GetPartnerEmailRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.email.v1.schema.GetPartnerEmailResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.email.v1.schema.WsPartnerEmailType
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.leistungserbringer.v1.schema.GetPublicLeistungserbringerRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.leistungserbringer.v1.schema.GetPublicLeistungserbringerResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.leistungserbringer.v1.schema.WsPublicLeistungserbringerType
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.oezuteilung.v1.schema.GetPartnerOeZuteilungRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.oezuteilung.v1.schema.GetPartnerOeZuteilungResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.organisationseinheit.v1.schema.GetOrganisationseinheitRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.organisationseinheit.v1.schema.GetOrganisationseinheitResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.organisationseinheit.v1.schema.WsOrganisationseinheitType
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partner.v1.schema.GetFamilieRequest
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partner.v1.schema.GetFamilieResponse
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partner.v1.schema.GetPartnerRequest
@@ -11,6 +46,21 @@ import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partneridentifik
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partneridentifikator.v1.schema.GetPartneridentifikatorResponse
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partneridentifikator.v1.schema.SearchOnlineIdRequest
 import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.partneridentifikator.v1.schema.SearchOnlineIdResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.GetOrganisationseinheitTelefonRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.GetOrganisationseinheitTelefonResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.GetPartnerTelefonRequest
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.GetPartnerTelefonResponse
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.WsOrganisationseinheitTelefonType
+import com.adcubum.syrius.api.partnermgmt.partnerdatenverw.data.telefon.v1.schema.WsPartnerTelefonType
+import com.adcubum.syrius.api.produktmgmt.common.identifier.v1.WsProduktIdType
+import com.adcubum.syrius.api.produktmgmt.produktpflege.produktkatalog.data.produkt.v1.schema.GetProduktRequest
+import com.adcubum.syrius.api.produktmgmt.produktpflege.produktkatalog.data.produkt.v1.schema.GetProduktResponse
+import com.adcubum.syrius.api.produktmgmt.produktpflege.produktkatalog.data.produkt.v1.schema.WsProduktType
+import com.adcubum.syrius.api.schadenleistungsmgmt.leistungserbringung.leistungsberechnung.data.kostenbeteiligung.v1.schema.ReadKostenbeteiligungenOfGrundversicherungForPartnerRequest
+import com.adcubum.syrius.api.schadenleistungsmgmt.leistungserbringung.leistungsberechnung.data.kostenbeteiligung.v1.schema.ReadKostenbeteiligungenOfGrundversicherungForPartnerResponse
+import com.adcubum.syrius.api.schadenleistungsmgmt.leistungserbringung.leistungsberechnung.data.kostenbeteiligung.v1.schema.ReadKostenbeteiligungenRequest
+import com.adcubum.syrius.api.schadenleistungsmgmt.leistungserbringung.leistungsberechnung.data.kostenbeteiligung.v1.schema.ReadKostenbeteiligungenResponse
+import com.adcubum.syrius.api.schadenleistungsmgmt.leistungserbringung.leistungsberechnung.data.kostenbeteiligung.v1.schema.WsKostenbeteiligungType
 import org.springframework.ws.WebServiceMessageFactory
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport
 import org.springframework.ws.client.support.interceptor.ClientInterceptor
@@ -27,30 +77,118 @@ class ApiBridgeClient(webServiceMessageFactory: WebServiceMessageFactory, defaul
 
     val partnerIdentifikatorService = defaultUri + "apibridge-partnermgmt/PartneridentifikatorService_v1"
     val partnerService = defaultUri + "apibridge-partnermgmt/PartnerService_v1"
+    val organisationseinheitService = defaultUri + "apibridge-partnermgmt/OrganisationseinheitService_v1"
+    val oeZuteilungService = defaultUri + "apibridge-partnermgmt/OeZuteilungService_v1"
+    val adresseService = defaultUri + "apibridge-partnermgmt/AdresseService_v1"
+    val emailService = defaultUri + "apibridge-partnermgmt/EmailService_v1"
+    val telefonService = defaultUri + "apibridge-partnermgmt/TelefonService_v1"
+    val vertragService = defaultUri + "apibridge-bestandsverw/VertragService_v1"
+    val versichertesObjektService = defaultUri + "apibridge-bestandsverw/VersichertesObjektService_v1"
+    val versichertenkarteService = defaultUri + "apibridge-bestandsverw/VersichertenkarteService_v1"
+    val produktService = defaultUri + "apibridge-produktmgmt/ProduktService_v1"
+    val kostenbeteiligungService = defaultUri + "apibridge-schadenleistungsmgmt/KostenbeteiligungService_v1"
+    val codeService = defaultUri + "apibridge-infrastructure/CodeService_v1"
+    val bankService = defaultUri + "apibridge-partnermgmt/BankService_v1"
+    val leistungserbringerService = defaultUri + "apibridge-partnermgmt/LeistungserbringerService_v1"
+
     val datatypeFactory = DatatypeFactory.newInstance()
 
-    fun resolvePortalKontoId(portalKontoId: String): String? {
+    fun resolvePortalKontoId(portalKontoId: String): WsPartnerIdType? {
         val searchOnlineIdRequest = SearchOnlineIdRequest(portalKontoId)
         val searchOnlineIdResponse = webServiceTemplate.marshalSendAndReceive(partnerIdentifikatorService, searchOnlineIdRequest) as SearchOnlineIdResponse
 
         if (searchOnlineIdResponse.partneridentifikatorId == null) {
             return null
         }
-        val getPartneridentifikatorRequest = GetPartneridentifikatorRequest(listOf(searchOnlineIdResponse.partneridentifikatorId), stichtag())
-        return (webServiceTemplate.marshalSendAndReceive(partnerIdentifikatorService, getPartneridentifikatorRequest) as GetPartneridentifikatorResponse).partneridentifikators.firstOrNull()?.run {
-            partnerId.id
-        }
+        val partneridentifikatorRequest = GetPartneridentifikatorRequest(listOf(searchOnlineIdResponse.partneridentifikatorId), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(partnerIdentifikatorService, partneridentifikatorRequest) as GetPartneridentifikatorResponse).partneridentifikators.firstOrNull()?.partnerId
+
     }
 
-    fun getPartner(partnerId: String): WsPartnerType {
-        val getPartnerRequest = GetPartnerRequest(listOf(WsPartnerIdType(partnerId)), stichtag())
-        return (webServiceTemplate.marshalSendAndReceive(partnerService, getPartnerRequest) as GetPartnerResponse).partners.first()
+    fun getPartner(partnerId: WsPartnerIdType): WsPartnerType {
+        val partnerRequest = GetPartnerRequest(listOf(partnerId), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(partnerService, partnerRequest) as GetPartnerResponse).partners.first()
     }
 
-    fun getFamilie(partnerId: String): WsFamilieType {
-        val getFamilieRequest = GetFamilieRequest(listOf(WsPartnerIdType(partnerId)), stichtag())
-        return (webServiceTemplate.marshalSendAndReceive(partnerService, getFamilieRequest) as GetFamilieResponse).families.first()
+    fun getFamilie(partnerId: WsPartnerIdType): WsFamilieType {
+        val familieRequest = GetFamilieRequest(listOf(partnerId), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(partnerService, familieRequest) as GetFamilieResponse).families.first()
     }
 
-    fun stichtag() = datatypeFactory.newXMLGregorianCalendar(GregorianCalendar())
+    fun getDomizilAdressse(vararg partnerIds: WsPartnerIdType): List<WsPartnerDomiziladresseType> {
+        val adressRequest = GetPartnerDomiziladresseRequest(listOf(*partnerIds), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(adresseService, adressRequest) as GetPartnerDomiziladresseResponse).partnerDomiziladressens
+    }
+
+    fun getTelefon(vararg partnerIds: WsPartnerIdType): List<WsPartnerTelefonType> {
+        val telefonRequest = GetPartnerTelefonRequest(listOf(*partnerIds), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(telefonService, telefonRequest) as GetPartnerTelefonResponse).partnerTelefons
+    }
+
+    fun getEmail(vararg partnerIds: WsPartnerIdType): List<WsPartnerEmailType> {
+        val emailRequest = GetPartnerEmailRequest(listOf(*partnerIds), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(emailService, emailRequest) as GetPartnerEmailResponse).partnerEmails
+    }
+
+    fun getOrganisationseinheit(vararg partnerIds: WsPartnerIdType): List<WsOrganisationseinheitType> {
+        val oeZuteilungRequest = GetPartnerOeZuteilungRequest(listOf(*partnerIds), stichtag())
+        val zuteilungList = (webServiceTemplate.marshalSendAndReceive(oeZuteilungService, oeZuteilungRequest) as GetPartnerOeZuteilungResponse).partnerGbereichListOeZuteilungs
+
+        val organisationseinheitRequest = GetOrganisationseinheitRequest(zuteilungList.flatMap { it.partnerGbereichOeZuteilungs }.flatMap { it.oeZuteilungs }.map { it.organisationseinheitId }, stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(organisationseinheitService, organisationseinheitRequest) as GetOrganisationseinheitResponse).organisationseinheits
+    }
+
+    fun getOrganisationseinheitTelefon(vararg organisationseinheitIds: WsOrganisationseinheitIdType): List<WsOrganisationseinheitTelefonType> {
+        val organisationseinheitTelefonRequest = GetOrganisationseinheitTelefonRequest(listOf(*organisationseinheitIds), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(telefonService, organisationseinheitTelefonRequest) as GetOrganisationseinheitTelefonResponse).organisationseinheitTelefons
+    }
+
+    fun getVertrag(partnerId: com.adcubum.syrius.api.bestandsverw.common.identifier.v1.WsPartnerIdType): List<WsVertragType> {
+        val searchVertragForPartnerRequest = SearchVertragForPartnerRequest(partnerId, stichtag())
+        val vertragIdList = (webServiceTemplate.marshalSendAndReceive(vertragService, searchVertragForPartnerRequest) as SearchVertragForPartnerResponse).vertragIds
+
+        val vertragRequest = GetVertragRequest(vertragIdList, stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(vertragService, vertragRequest) as GetVertragResponse).vertrags
+    }
+
+    fun getVersichertesObjekt(partnerId: com.adcubum.syrius.api.bestandsverw.common.identifier.v1.WsPartnerIdType): List<WsVersichertesObjektType> {
+        val searchVersichertesObjektForPartnerRequest = SearchVersichertesObjektForPartnerRequest(partnerId, stichtag())
+        val versichertesObjektList = (webServiceTemplate.marshalSendAndReceive(versichertesObjektService, searchVersichertesObjektForPartnerRequest) as SearchVersichertesObjektForPartnerResponse).versichertesObjektIds
+
+        val versichertesObjektRequest = GetVersichertesObjektRequest(versichertesObjektList, stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(versichertesObjektService, versichertesObjektRequest) as GetVersichertesObjektResponse).versichertesObjekts
+    }
+
+    fun getProdukt(vararg produktIds: WsProduktIdType): List<WsProduktType> {
+        val produktRequest = GetProduktRequest(listOf(*produktIds), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(produktService, produktRequest) as GetProduktResponse).produkts
+    }
+
+    fun getVersichertenkarte(vararg partnerIds: com.adcubum.syrius.api.bestandsverw.common.identifier.v1.WsPartnerIdType): List<WsVersichertenkarteType> {
+        val searchAktuelleVersichertenkarteForPartnerRequest = SearchAktuelleVersichertenkarteForPartnerRequest(listOf(*partnerIds))
+        val versichertenkarteIdList = (webServiceTemplate.marshalSendAndReceive(versichertenkarteService, searchAktuelleVersichertenkarteForPartnerRequest) as SearchAktuelleVersichertenkarteForPartnerResponse).versichertenkarteIds
+
+        val versichertenkarteRequest = GetVersichertenkarteRequest(versichertenkarteIdList)
+        return (webServiceTemplate.marshalSendAndReceive(versichertenkarteService, versichertenkarteRequest) as GetVersichertenkarteResponse).versichertenkartes
+    }
+
+    fun getKostenbeteiligung(vararg partnerIds: com.adcubum.syrius.api.schadenleistungsmgmt.extern.partner.identifier.v1.WsPartnerIdType): List<WsKostenbeteiligungType> {
+        val kostenbeteiligungenIdRequest = ReadKostenbeteiligungenOfGrundversicherungForPartnerRequest(listOf(*partnerIds), stichtag())
+        val kostenbeteiligungIdList = (webServiceTemplate.marshalSendAndReceive(kostenbeteiligungService, kostenbeteiligungenIdRequest) as ReadKostenbeteiligungenOfGrundversicherungForPartnerResponse).kostenbeteiligungGrundversicherungs
+
+        val kostenbeteiligungenRequest = ReadKostenbeteiligungenRequest(kostenbeteiligungIdList.map { it.id }, stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(kostenbeteiligungService, kostenbeteiligungenRequest) as ReadKostenbeteiligungenResponse).kostenbeteiligungens
+    }
+
+    fun getKollektivvertrag(vararg kollektivvertragId: WsKollektivvertragNamentlichIdType): List<WsKollektivvertragNamentlichType> {
+        val kollektivvertragRequest = GetKollektivvertragNamentlichRequest(listOf(*kollektivvertragId), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(vertragService, kollektivvertragRequest) as GetKollektivvertragNamentlichResponse).kollektivvertragNamentliches
+    }
+
+    fun getLeistungserbringer(vararg leistungserbringerId: WsLeistungserbringerIdType): List<WsPublicLeistungserbringerType> {
+        val leistungserbringerRequest = GetPublicLeistungserbringerRequest(listOf(*leistungserbringerId), stichtag())
+        return (webServiceTemplate.marshalSendAndReceive(leistungserbringerService, leistungserbringerRequest) as GetPublicLeistungserbringerResponse).publicLeistungserbringers
+    }
+
+    private fun stichtag() = datatypeFactory.newXMLGregorianCalendar(GregorianCalendar())
 }
